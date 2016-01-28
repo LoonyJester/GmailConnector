3d89e35cdd38409b476285fa6c6046ddd61afe2e
Alex M. <mykhaylichenko@zfort.com>
2016-01-28 18:26:59 +0200

    signalr test

--- GmailConnectorExtention/scripts/main.js
+++ GmailConnectorExtention/scripts/main.js
@@ -30,7 +30,42 @@ InboxSDK.load('1.0', 'sdk_LoonyTestSdk2_e0399f0675').then(function (sdk) {
 	    '</div> ' +
 	    '</div>';
 
+    //// SIGNALR #################################################
+    // Declare a proxy to reference the hub.
+	var chat = $.connection.signalRChat;
+    // Create a function that the hub can call to broadcast messages.
+	chat.client.broadcastMessage = function (name, message) {
+	    // Html encode display name and message.
+//	    var encodedName = $('<div />').text(name).html();
+//	    var encodedMsg = $('<div />').text(message).html();
+	    // Add the message to the page.
+	    alert('Name: ' + $('<div />').text(name).html() + '. Message: ' + $('<div />').text(message).html());
+
+//	    $('#discussion').append('<li><strong>' + encodedName
+//            + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+	};
+    // Get the user name and store it to prepend to messages.
+//	$('#displayname').val(prompt('Enter your name:', ''));
+    // Set initial focus to message input box.
+//	$('#message').focus();
+    // Start the connection.
+	$.connection.hub.start().done(function () {
+	    alert('$.connection.hub.start().done(...)');
+//	    $('#sendmessage').click(function () {
+//	        // Call the Send method on the hub.
+//	        chat.server.send($('#displayname').val(), $('#message').val());
+//	        // Clear text box and reset focus for next comment.
+//	        $('#message').val('').focus();
+//	    });
+	});
+
+    //// SIGNALR #################################################
+
+
+
 	sdk.Lists.registerThreadRowViewHandler(function (threadRowView) {
+
+
 
 	    var min = 0;
 	    var max = 3;
