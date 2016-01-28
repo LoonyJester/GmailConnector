f60ab3fa7ca873ebfc9429a4de5528075e440517
Alex M. <mykhaylichenko@zfort.com>
2016-01-28 14:40:14 +0200

    add backend project with test SignalR functionality

---
+++ backend/App_Start/SignalRChat.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup(typeof(backend.SignalRChat))]
+namespace backend
+{
+    public class SignalRChat
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ backend/Utils/SignalRChat.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace backend.Utils
+{
+    public class SignalRChat : Hub
+    {
+
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
